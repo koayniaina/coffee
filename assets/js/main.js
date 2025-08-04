a55845006d76b3dd948313d3d6e49e0e0fb8b788
Koayniaina <setraniaina08@gamil.com>
2025-08-04 21:40:10 +0200

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
 const burger = document.querySelector(".burger");
-// const nav = document.querySelector(".nav");
+const nav = document.querySelector(" .nav-links span");
+const navMenu = document.querySelector(" .nav-links li");
 const navLinks = document.querySelector(".nav-links");
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+nav.addEventListener("click", () => {
+  burger.click();
+});
+
+navMenu.addEventListener("click", () => {
+  burger.click();
 });
