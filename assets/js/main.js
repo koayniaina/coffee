7988d61a048378a8f6ad5959a710efe3093ff938
Koayniaina <setraniaina08@gamil.com>
2025-08-04 21:16:35 +0200

    coffee repository

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger");
+// const nav = document.querySelector(".nav");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
